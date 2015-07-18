bb366d4d09e01ff01fe5b03c41051bb18b7fe16d
Daniyar Itegulov <wibkwibk@gmail.com>
2015-07-18 19:47:19 +0500

    Added new test, changes access for DeBruijn to package-private

--- src/test/scala/com/itegulov/typetheory/lambda/LambdaSubstituteSpec.scala
+++ src/test/scala/com/itegulov/typetheory/lambda/LambdaSubstituteSpec.scala
@@ -12,6 +12,9 @@ class LambdaSubstituteSpec extends FlatSpec with Matchers {
     substitute(apply("a"), Var("a"), apply("b c")) shouldEqual apply("b c")
     substitute(apply("λa.(a b)"), Var("b"), apply("b c")) shouldEqual apply("λa.(a (b c))")
     intercept[NoFreeSubstitutionException] {
+      substitute(apply("λx.x f z λf.f λz.z f f"), Var("f"), apply("λg.z x")) shouldEqual apply("λa.(a (b c))")
+    }
+    intercept[NoFreeSubstitutionException] {
       substitute(apply("λa.a"), Var("a"), apply("b"))
     }
   }
