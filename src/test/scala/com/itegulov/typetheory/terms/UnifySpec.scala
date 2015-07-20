a7d0bcc4e6085637e430583ae2bbf58acbd5d68e
Daniyar Itegulov <wibkwibk@gmail.com>
2015-07-20 13:45:14 +0500

    Added one test for unification

--- src/test/scala/com/itegulov/typetheory/terms/UnifySpec.scala
+++ src/test/scala/com/itegulov/typetheory/terms/UnifySpec.scala
@@ -9,10 +9,15 @@ import scala.collection.mutable.ArrayBuffer
  */
 class UnifySpec extends FlatSpec with Matchers {
   "Unifier" should "correctly unify" in {
-    println(addIDs(unify(ArrayBuffer(
+    addIDs(unify(ArrayBuffer(
       TermEq(TFun("g", List(TVar("x2"))), TVar("x1")),
       TermEq(TFun("f", List(TVar("x1"), TFun("h", List(TVar("x1"))), TVar("x2"))),
              TFun("f", List(TFun("g", List(TVar("x3"))), TVar("x4"), TVar("x3"))))
-    ))))
+    ))) shouldEqual ArrayBuffer(
+      TermEq(TVar("x2"), TVar("x2")),
+      TermEq(TVar("x4"), TFun("h", List(TFun("g", List(TVar("x2")))))),
+      TermEq(TVar("x1"), TFun("g", List(TVar("x2")))),
+      TermEq(TVar("x3"), TVar("x2"))
+    )
   }
 }
